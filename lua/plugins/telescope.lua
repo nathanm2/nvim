75e635434cef5725a78b9a854097e826f0b7d8de
Nathan Miller <Nathan.Miller@wdc.com>
2023-06-15 21:41:58 -0500

    Telescope mappings

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,9 +2,14 @@ return {
   { "nvim-telescope/telescope.nvim",
     version = "0.1.x",
     dependencies = { "nvim-lua/plenary.nvim" },
+    lazy = false,
     keys = {
       { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files" },
-      { "<leader>fg", "<cmd>Telescope git_files<cr>", desc = "Find git files" },
+      { "<leader>fF", "<cmd>Telescope find_git<cr>", desc = "Find files" },
+      { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
+      { "<leader>fs", "<cmd>Telescope grep_string<cr>", desc = "Find string" },
+      { "<leader>fr", "<cmd>Telescope lsp_references<cr>", desc = "LSP references" },
+      { "<leader>fc", "<cmd>Telescope lsp_incoming_calls<cr>", desc = "LSP incoming calls" },
     }
   }
 }
