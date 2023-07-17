222a3194e87c38c54dca4961f853f0cd1df6d612
Nathan Miller <Nathan.Miller@wdc.com>
2023-07-17 15:43:22 -0500

    Lualine + Bufferline

---
+++ lua/plugins/ui.lua
@@ -0,0 +1,22 @@
+return {
+  { "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 500
+    end,
+  },
+  { "nvim-lualine/lualine.nvim",
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    opts = {},
+  },
+  { 'akinsho/bufferline.nvim',
+    version = "*",
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    opts = {
+      options = {
+        numbers = "both",
+      }
+    },
+  },
+}
